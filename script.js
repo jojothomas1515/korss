f863d71734a3fac4f4d8a166aeed5f380e4014f3
jojothomas1515 <jojothomas1515@gmail.com>
2022-04-28 05:32:21 +0100

    added to

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-const close = document.getElementById("close");
-const modal = document.getElementById("modal");
-const open = document.getElementsByClassName("btn");
+const info = document.getElementById('whtths')
+const whatthis = document.getElementById('info-button')
 
-close.addEventListener("click", (e) => {
-  modal.style.display = "none";
-});
-
-open[0].addEventListener("click", () => {
-  modal.style.display = "block";
-});
-
-window.onload = () => {
-  setTimeout(() => {
-    modal.style.display = "block";
-    alert("please subscribe you cunt");
-  }, 10000);
-};
+whatthis.addEventListener('click',  (e)=>{
+  e.preventDefault()
+  info.classList.toggle('dis')
+ 
+  
+})
